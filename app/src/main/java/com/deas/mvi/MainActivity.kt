71e76801ab1b011968daf9ed7fc8b548c8f973e9
I Gede Agus Maha Putra <igede@Is-MacBook-Pro.local>
2022-05-12 08:22:57 +0700

    add module and setup navigation

---
+++ app/src/main/java/com/deas/mvi/MainActivity.kt
@@ -0,0 +1,31 @@
+package com.deas.mvi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
+import com.deas.mvi.theme.ComposeBaseTheme
+import dagger.hilt.android.AndroidEntryPoint
+import com.deas.navigation.Screen
+import com.deas.navigation.base.loadMultiNavComposable
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContent {
+            val navController = rememberNavController()
+            ComposeBaseTheme {
+                NavHost(
+                    navController = navController,
+                    startDestination = Screen.Home.name
+                ) {
+                    loadMultiNavComposable(navController)
+                }
+
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/deas/mvi/theme/Type.kt
@@ -0,0 +1,26 @@
+package com.deas.mvi.theme
+
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.sp
+
+val Typography = Typography(
+    body1 = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 16.sp
+    )
+    /* Other default text styles to override
+    button = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.W500,
+        fontSize = 14.sp
+    ),
+    caption = TextStyle(
+        fontFamily = FontFamily.Default,
+        fontWeight = FontWeight.Normal,
+        fontSize = 12.sp
+    )
+    */
+)
